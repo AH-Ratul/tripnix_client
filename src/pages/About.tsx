6d523e67e2222c5ceda90bf49541b315de6f0f2b
AH-Ratul <ahratul21@gmail.com>
2025-08-09 17:35:41 +0600

    feat: about, login, register pages added

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+
+
+const About = () => {
+    return (
+        <div className="container mx-auto py-10 px-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa inventore quibusdam esse libero quas reiciendis, modi enim labore repellendus quod. Ipsam maiores, dolore repellendus odit officiis recusandae expedita. Perferendis, doloremque.
+        </div>
+    );
+};
+
+export default About;
