81c463f6a21b49ad8d93101ba8755b8c7167c03b
AH-Ratul <ahratul21@gmail.com>
2025-08-09 17:33:04 +0600

    feat: login & register route added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,34 @@
 import App from "@/App";
+import AdminLayout from "@/components/layout/AdminLayout";
+import About from "@/pages/About";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: App,
+    children: [
+        {
+            path: "about",
+            Component: About
+        }
+    ]
   },
+  {
+    path: "/admin",
+    Component: AdminLayout,
+    children: [
+
+    ]
+  },
+  {
+    path: "/login",
+    Component: Login
+  },
+  {
+    path: "/register",
+    Component: Register
+  }
 ]);
