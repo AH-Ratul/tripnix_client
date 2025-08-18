014baff5294bec0b60f246396e6cdadc191b51b9
AH-Ratul <ahratul21@gmail.com>
2025-08-18 18:51:45 +0600

    feat: manage admin side bar items

---
+++ src/routes/adminSidebarItems.ts
@@ -0,0 +1,28 @@
+import type { IRouteItem } from "@/types";
+import { lazy } from "react";
+
+const Analytics = lazy(() => import("@/pages/Admin/Analytics"));
+const AddTour = lazy(() => import("@/pages/Admin/AddTour"));
+
+export const adminSidebarItems: IRouteItem[] = [
+  {
+    title: "Dashboard",
+    items: [
+      {
+        title: "Analytics",
+        url: "/admin/analytics",
+        component: Analytics,
+      },
+    ],
+  },
+  {
+    title: "Tour Management",
+    items: [
+      {
+        title: "Add Tour",
+        url: "/admin/add-tour",
+        component: AddTour,
+      },
+    ],
+  },
+];
