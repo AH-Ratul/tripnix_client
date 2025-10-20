175a34b8e388ada22fe97b69e9c9cb2b857db72e
AH-Ratul <ahratul21@gmail.com>
2025-10-20 20:19:27 +0600

    feat: add tour & destination card section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,29 @@
 import { HeroSection } from "@/components/modules/HomePage/HeroSection";
+import Tours from "./Tours";
+import { Link } from "react-router";
+import PopularDestination from "@/components/modules/HomePage/popularDestination/PopularDestination";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+
+      {/* Tour Section */}
+      <div className="container mx-auto px-3 lg:px-0 mt-20">
+        <div className="flex justify-between items-center mb-5">
+          <p className="text-3xl font-medium font-marcellus">Recent Tours</p>
+          <Link
+            to={""}
+            className="hover:text-coquelicot border-b border-black hover:border-coquelicot transition-normal duration-300 ease-in-out"
+          >
+            View All Tours
+          </Link>
+        </div>
+
+        <Tours />
+      </div>
+
+      <PopularDestination />
     </div>
   );
 };
