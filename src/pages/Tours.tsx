64735b94f2c631091e68c06b3dd4cf1609bf1157
AH-Ratul <ahratul21@gmail.com>
2025-10-30 22:24:49 +0600

    feat: get params from search params

--- src/pages/Tours.tsx
+++ src/pages/Tours.tsx
@@ -1,16 +1,32 @@
 import TourCard from "@/components/modules/TourPage/TourCard";
+import TourFilters from "@/components/modules/TourPage/TourFilters";
 import Loader from "@/components/shared/Loader/Loader";
 import { useGetAllTourQuery } from "@/redux/features/tour/tour.api";
+import { useSearchParams } from "react-router";
 
 const Tours = () => {
-  const { data, isLoading } = useGetAllTourQuery(undefined);
+  const [searchParams] = useSearchParams();
+
+  const division = searchParams.get("division") || undefined;
+  const tourType = searchParams.get("tourType") || undefined;
+
+  const { data, isLoading } = useGetAllTourQuery({
+    division,
+    tourType,
+  });
 
   if (isLoading) {
     return <Loader />;
   }
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
-      <TourCard tourData={data} />
+    <div className="container mx-auto px-3 my-8 ">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-marcellus mb-5 font-bold">All Tours</h1>
+        <TourFilters />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
+        <TourCard tourData={data} />
+      </div>
     </div>
   );
 };
