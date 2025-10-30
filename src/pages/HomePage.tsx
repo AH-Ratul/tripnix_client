1c4071e9b7c9988cc162ea58b218830cd501dffc
AH-Ratul <ahratul21@gmail.com>
2025-10-30 22:25:26 +0600

    update: tour card section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,16 @@
 import { HeroSection } from "@/components/modules/HomePage/HeroSection";
-import Tours from "./Tours";
 import { Link } from "react-router";
 import PopularDestination from "@/components/modules/HomePage/popularDestination/PopularDestination";
+import TourCard from "@/components/modules/TourPage/TourCard";
+import { useGetAllTourQuery } from "@/redux/features/tour/tour.api";
+import Loader from "@/components/shared/Loader/Loader";
 
 const HomePage = () => {
+  const { data, isLoading } = useGetAllTourQuery(undefined);
+
+  if (isLoading) {
+    return <Loader />;
+  }
   return (
     <div>
       <HeroSection />
@@ -20,7 +27,9 @@ const HomePage = () => {
           </Link>
         </div>
 
-        <Tours />
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
+          <TourCard tourData={data} />
+        </div>
       </div>
 
       <PopularDestination />
