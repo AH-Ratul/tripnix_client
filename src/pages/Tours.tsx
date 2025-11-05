78f809141a6a2381f756755e5dff1f783f6866d1
AH-Ratul <ahratul21@gmail.com>
2025-11-05 21:05:41 +0600

    feat: add pagination & update data

--- src/pages/Tours.tsx
+++ src/pages/Tours.tsx
@@ -3,8 +3,18 @@ import TourFilters from "@/components/modules/TourPage/TourFilters";
 import Loader from "@/components/shared/Loader/Loader";
 import { useGetAllTourQuery } from "@/redux/features/tour/tour.api";
 import { useSearchParams } from "react-router";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { useState } from "react";
 
 const Tours = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const [searchParams] = useSearchParams();
 
   const division = searchParams.get("division") || undefined;
@@ -13,7 +23,11 @@ const Tours = () => {
   const { data, isLoading } = useGetAllTourQuery({
     division,
     tourType,
+    limit: 9,
+    page: currentPage,
   });
+  
+  const totalPage = data?.meta?.totalPage;
 
   if (isLoading) {
     return (
@@ -23,14 +37,46 @@ const Tours = () => {
     );
   }
   return (
-    <div className="container mx-auto px-3 my-8 ">
+    <div className="container mx-auto px-3 my-8">
       <div className="flex flex-col lg:flex-row  gap-6">
         <TourFilters />
 
         <div>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-            <TourCard tourData={data} />
+            <TourCard tourData={data?.data} />
           </div>
+        </div>
+      </div>
+
+      <div className="flex justify-end mt-10">
+        <div>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  className={
+                    currentPage === 1
+                      ? "pointer-events-none opacity-45"
+                      : "cursor-pointer"
+                  }
+                  onClick={() => setCurrentPage((prev) => prev - 1)}
+                />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink>{currentPage}</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext
+                  className={
+                    currentPage === totalPage
+                      ? "pointer-events-none opacity-45"
+                      : "cursor-pointer"
+                  }
+                  onClick={() => setCurrentPage((prev) => prev + 1)}
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </div>
     </div>
